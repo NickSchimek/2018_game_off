72cb546dbb25c6bc26e07a186f092eb60daa4be5
Nick Schimek <cfme00@gmail.com>
2018-11-06 13:45:09 -0800

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    connect: {
+      server: {
+        options: {
+          port: 4000, // custom port
+          base: '.', // current directory for 'index.html' is root
+          keepalive: true, // keep the server alive indefinitely
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.registerTask('default');
+};
+
+
